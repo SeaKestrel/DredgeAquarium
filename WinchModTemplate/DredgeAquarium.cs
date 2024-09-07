1267bbd5b91fd4f70b1c37c80041fc24d6dd4d95
SeaKestrel <jepantal@outlook.fr>
2024-09-07 11:28:45 +0200

    names

---
+++ WinchModTemplate/DredgeAquarium.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace WinchModTemplate
+{
+	public class DredgeAquarium : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(DredgeAquarium)} has loaded!");
+		}
+	}
+}
